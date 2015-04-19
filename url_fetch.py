1252be70385c161495e3c4bb881d419eca4164b8
Kurush Dubash <dubash.kurush@gmail.com>
2015-04-18 23:15:12 -0700

    Re-wrote the shortcode Algorithm

--- url_fetch.py
+++ url_fetch.py
@@ -1,12 +1,9 @@
 import requests
 import math
-from helper import*
+import random
 
+values = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-BASE = 122
-chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-Chars = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-nums  = str([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 def strip_url(url):
 	string = ''
@@ -17,37 +14,15 @@ def strip_url(url):
 	print(string)
 	return string
 
-def map_nums_to_spec(arr):
-	string = ''
-	for item in arr:
-		string += chr(item)
-	return string
+def generate_short_code():
+	code = ''
+	for num in range(6):
+		code += values[random.randrange(0, 63)]
+	return code
 
 def find_short_url(url):
-	#url = convert_to_base(url)
-
-	# google.com/email
-	# berkeleybside.com/fox-show-1/
-	# lukesheard.com/about/i-love-you-kush
-	# berkeleybside.com/about/team
-	test = saturate(url)
-	print(test[1])
-	return_url = map_nums_to_spec(test[1])
-	return test[0]
-
-def convert_to_base(url):
-	base_value = []
-
-
-	url = url[::-1]
-
-	int_sum=0
-	counter = math.log(url,BASE) + 1
+	short_code = generate_short_code()
 	
+	return short_code
 
-	for idx, char in enumerate(url):
-		int_sum += true_ord(char) * int(math.pow(BASE, idx))
-	return base_value[::-1]
 
-def true_ord(char):
-	return 0
