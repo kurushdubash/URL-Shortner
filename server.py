38a89824417c725b2c391a4b555d5f2d0b316eb6
Kurush Dubash <dubash.kurush@gmail.com>
2015-04-18 20:27:58 -0700

    Cleaned up unneeded files

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 from random import randint
 import os
-from data import*
+from url_fetch import*
 app = Flask(__name__)
 
 
@@ -10,43 +10,16 @@ def hello():
     return app.send_static_file('index.html')
 
 
-@app.route("/api/lottery", methods=["POST"])
-def update_data():
-    print(request.form['value'])
-    val = request.form['value']
-    if(val == 'NY'):
-        ny = 'checked'
-        pb = ''
-    else:
-        ny = ''
-        pb = 'checked'
-
-    data = frequency_winning_numbers(check(val))
-    # mega = frequency_mega(check1(val))
-    # bm = best_mega(mega)
-    #data = [tester([]),tester([])]
+@app.route("/shorten", methods=["POST"])
+def get_website():
+    print(request.form['url'])
+    url = request.form['url']
 
 
-    print (data)
-    print(len(data))
-    best = best_five(data)
-
-    listoffive = best
-
-    if(len(listoffive) > 0):
-        prob = find_probability( probability(data), listoffive)
-    else:
-        prob = ''
+    short_url = find_short_url(url)
 
 
-    return render_template('data.html', data=data, best=best, size=len(data), ny=ny, pb=pb, prob=prob)
-
-def tester(data):
-    x = 0
-    while x < 25:
-        data.append(randint(1, 25))
-        x+=1
-    return data
+    return render_template('shorten.html')
 
 if __name__ == "__main__":
     app.debug = True
