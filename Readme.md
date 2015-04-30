b0ae8c04d96299f655c65c7670a1a0f4b1101e04
Luke Sheard <me@lukesheard.com>
2015-04-29 19:46:26 -0700

    Server and ReadMe updates

--- Readme.md
+++ Readme.md
@@ -1,2 +1,10 @@
 #Berkeley B-Side URL Shotener
-### v0.1
+### v1.0
+
+## Basics
+This will shorten a url to the heroku app's address and forward you on. Very simple and quick works for all URLs and avoids Bit.Ly. Gives complete control over the URLs. 
+
+## To-Do
+- Checking for URLs in Database
+- Custom URL per user
+- Custom URL forwarding
--- server.py
+++ server.py
@@ -1,34 +1,34 @@
 from flask import *
 from random import randint
 import os
-from url_fetch import *
+
+# Custom URL and Database handlers
+import url_fetch as url
 import database as db
+
 app = Flask(__name__)
 
-
+# New User Login
 @app.route("/")
-def hello():
+def welcome():
     return render_template('index.html', return_url="")
 
-
+# Created a shortened URL
 @app.route("/", methods=["POST"])
-def get_website():
+def added_website():
     print(request.form['url'])
     url = request.form['url']
-
-
     stripped_url = strip_url(url[url.index(".") + len("."):])
-    bside = "http://bs.id/"
     current = request.url_root
     short_url = current + str(find_short_url(url, stripped_url))
 
     return render_template('index.html', return_url=short_url)
 
+# Returning a shortened URL
 @app.route("/<url>/")
 def forward(url):
 	send = db.check_database(url)
 	return redirect(send)
-
 
 if __name__ == "__main__":
     app.debug = True
